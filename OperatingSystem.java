4c94f9007d6f6d9b275375fd88f843c11178f575
Omar Fekry <67840386+omarafekry@users.noreply.github.com>
2023-06-04 21:57:28 +0200

    done

--- OperatingSystem.java
+++ OperatingSystem.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.io.*;
 
 public class OperatingSystem {
@@ -6,6 +7,7 @@ public class OperatingSystem {
     Memory memory = new Memory();
     int programCount = 1;
     boolean doneReadingPrograms = false;
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args){
         OperatingSystem os = new OperatingSystem();
@@ -14,30 +16,77 @@ public class OperatingSystem {
 
     public void run(){
         while(true){
-
+            System.out.println();
+            System.out.println();
             if (!doneReadingPrograms) loadProgramToMemory("Program_" + programCount + ".txt");
             if (readyQueue.isEmpty()) return;
-
+            printQueue();
             Process process = readyQueue.removeFirst();
             memory.prepareProcess(process);
-
+            System.out.println("Currently executing process " + process.id);
             //run process here and set isDone property
+            for (int i = 0; i < 2 && !process.isDone; i++){
+                process.run();
+                memory.printMemory();
+            }
+            
 
             if (!process.isDone) readyQueue.addLast(process);
             else memory.removeProcess(process);
         }
     }
-
+    public void printQueue(){
+        int qsize = readyQueue.size();
+            System.out.print("Ready Queue:");
+            for (int i = 0; i < qsize; i++) {
+                Process p = readyQueue.removeFirst();
+                System.out.print(" " + p.id);
+                readyQueue.addLast(p);
+            }
+            System.out.println();
+    }
     public void loadProgramToMemory(String filename){
         File programFile = new File(filename);
         if (!programFile.exists()){
             doneReadingPrograms = true;
             return;
         }
-        Process process = memory.addProcess(programFile, programCount);
+        Process process = new Process(programCount, memory, this);
+        memory.addProcess(programFile, process);
         programCount++;
-        readyQueue.addLast(process);
+        readyQueue.addFirst(process);
     }
-
+    
+    public String readInput(){
+        String input = sc.nextLine();
+        return input;
+    }
+    public void printData(String data){
+        System.out.println(data);
+    }
+    public String readFromDisk(String filePath){
+        Scanner sc = new Scanner(filePath);
+        String line = sc.nextLine();
+        sc.close();
+        return line;
+    }
+    public void writeToDisk(String filePath, String data){
+        File file = new File(filePath);
+        try {
+            if (!file.exists())
+                file.createNewFile();
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.write(data);
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String readFromMemory(int id, String var){
+        return memory.getProcessVariable(id, var);
+    }
+    public void writeToMemory(int id, String var, String data){
+        memory.setProcessVariable(id, var, data);
+    }
     
 }
--- Process.java
+++ Process.java
@@ -1,4 +1,77 @@
+
 public class Process {
     int id;
+    Memory memory;
+    OperatingSystem os;
     boolean isDone = false;
+    public Process(int id, Memory memory, OperatingSystem os){
+        this.id = id;
+        this.memory = memory;
+        this.os = os;
+    }
+    public void run() {
+        int pc = memory.getProcessPC(id);
+        String instruction = memory.getProcessLine(id, pc);
+        System.out.println("Currently executing instruction " + pc + ": " + instruction);
+        String[] words = instruction.split(" ");
+        switch(words[0]){
+            case "print":
+                os.printData(getValue1(words));
+                break;
+            case "assign":
+                os.writeToMemory(id, getValue1(words), getValue2(words));
+                break;
+            case "writeFile":
+                os.writeToDisk(System.getProperty("user.dir") + "\\" + getValue1(words), getValue2(words));
+                break;
+            case "printFromTo":
+                int value1 = Integer.parseInt(getValue1(words)), value2 = Integer.parseInt(getValue2(words));
+                for (int i = value1; i <= value2; i++)
+                    os.printData("" + i);
+                break;
+        }
+
+        memory.setProcessPC(id, pc + 1);
+        if (memory.getProcessPC(id) == memory.getProcessLength(id)){
+            memory.setProcessState(id, "Finished");
+            isDone = true;
+        }
+    }
+    private String getValue2(String[] parts) {
+        int value2Start = 0;
+        boolean foundFirst = false;
+        for (int i = 1; i < parts.length; i++) 
+            if (!parts[i].equals("input") && !parts[i].equals("readFile") && !foundFirst)
+                foundFirst = true;
+            else if (foundFirst){
+                value2Start = i;
+                break;
+            }
+        
+
+        switch(parts[value2Start]){
+            case "input":
+                return os.readInput();
+            case "readFile":
+                return os.readFromDisk(System.getProperty("user.dir") + "\\" + parts[value2Start+1]);
+            default:
+                String value = os.readFromMemory(id, parts[value2Start]);
+                if (value == null) 
+                    return parts[value2Start];
+                return value;
+        }
+    }
+    public String getValue1(String[] parts){
+        switch(parts[1]){
+            case "input":
+                return os.readInput();
+            case "readFile":
+                return os.readFromDisk(System.getProperty("user.dir") + "\\" + parts[2]);
+            default:
+                String value = os.readFromMemory(id, parts[1]);
+                if (value == null) 
+                    return parts[1];
+                return value;
+        }
+    }
 }
